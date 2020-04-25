8753d2f0285babbf55fc7929a1a50bbde2648830
lifeonearth <61416721+lifeonearth@users.noreply.github.com>
2020-04-25 13:45:29 +0900

    수업+최종꾸미기
    수업+최종꾸미기

---
+++ weather.js
@@ -0,0 +1,61 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "3059d3f79faa5452093515faf9e4c2ff";
+const COORDS = "coords";
+
+function getWeather(lat, lon) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerHTML = `${temperature}℃ @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude: latitude,
+    longitude: longitude,
+  };
+  //   const coordsObj = {
+  //     latitude,
+  //     longitude,
+  //   };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
